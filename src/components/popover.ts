3bca53ef8ff6f63a9f4c9f4054fa0097cfc0af75
Gunjan Datta <me@dattabase.com>
2018-08-30 12:31:09 -0400

    Updated the popover to use the "container" option.

--- src/components/popover.ts
+++ src/components/popover.ts
@@ -21,6 +21,7 @@ export const Popover = (props: IPopoverProps): IPopover | string => {
     let el = document.createElement("div");
     let btnProps = props.btnProps || {};
     btnProps.el = el;
+    btnProps.isLink = props.isDismissible ? true : false;
     btnProps.toggle = "popover";
     btnProps.trigger = "focus";
     Button(btnProps);
@@ -53,8 +54,25 @@ export const Popover = (props: IPopoverProps): IPopover | string => {
     }
 
     // Set the attributes
+    el.children[0].setAttribute("tabindex", "0");
     el.children[0].setAttribute("title", options.title || "");
     el.children[0].setAttribute("data-content", options.content || "");
+
+    // Ensure the main popover element exists
+    // This will ensure the popovers are wrapped with a parent element with the "bs" class applied to it.
+    let elParent = document.querySelector("#bs-popovers");
+    if (elParent == null) {
+        // Create the main element
+        elParent = document.createElement("div");
+        elParent.classList.add("bs");
+        elParent.id = "bs-popovers";
+
+        // Add it to the page
+        document.body.appendChild(elParent)
+    }
+
+    // Set the options to target the main popover element
+    options.container = "#bs-popovers";
 
     // See if the element exists
     if (props.el) {
@@ -64,67 +82,8 @@ export const Popover = (props: IPopoverProps): IPopover | string => {
         // Set the html
         props.el.innerHTML = el.innerHTML;
 
-        // Set the popover to be manual
-        options.trigger = "manual";
-
         // Create the popover
         let popover = jQuery(props.el.children[0]).popover(options);
-
-        // Set a click event to manually toggle the popover
-        props.el.children[0].addEventListener("click", ev => {
-            // Prevent the window click event to be triggered
-            ev.stopPropagation();
-
-            // Toggle the popover
-            popover.popover("toggle");
-        });
-
-        // See if this popover is dismissible
-        if (props.isDismissible) {
-            // Set a click event
-            window.addEventListener("click", () => {
-                // See if the popover is open
-                if (props.el.getAttribute("data-visible") == "true") {
-                    // Hide the popover
-                    popover.popover("toggle");
-                }
-            });
-        }
-
-        // Set a pop over shown event to move the popover under a "bootstrap" element for the css to be applied
-        jQuery(popover).on("shown.bs.popover", () => {
-            // Set the attribute
-            props.el.setAttribute("data-visible", "true");
-
-            // Get the popover
-            let elId = popover[0].getAttribute("aria-describedby");
-            if (elId) {
-                // Get the element
-                let el = document.querySelector("#" + elId);
-                if (el) {
-                    // Get the popovers main element
-                    let elParent = document.querySelector("#bs-popovers");
-                    if (elParent == null) {
-                        // Create the main element
-                        elParent = document.createElement("div");
-                        elParent.classList.add("bs");
-                        elParent.id = "bs-popovers";
-
-                        // Add it to the page
-                        document.body.appendChild(elParent)
-                    }
-
-                    // Append the element
-                    elParent.appendChild(el);
-                }
-            }
-        });
-
-        // Set a popover hidden event to set the flag
-        jQuery(popover).on("hidden.bs.popover", () => {
-            // Set the attribute
-            props.el.setAttribute("data-visible", "false");
-        });
 
         // Return the popover
         return {
