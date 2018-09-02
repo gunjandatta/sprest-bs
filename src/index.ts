8ea63b4519a1782755691a04797db75fb81e897d
Gunjan Datta <me@dattabase.com>
2018-09-01 23:48:30 -0400

    Fixed the global variable not containing the core gd-sprest library.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import "bootstrap";
 import "./styles.scss";
-import * as $REST from "./rest";
+import { Components, jQuery, WebParts } from "./rest";
+export * from "gd-sprest";
 export * from "./rest";
 
 // See if the window exists
 if (window) {
-    // Set the global variable
-    window["$REST"] = $REST;
+    // Update the $REST global variable
+    if (window["$REST"]) {
+        window["$REST"].Component = Components;
+        window["$REST"].jQuery = jQuery;
+        window["$REST"].WebParts = WebParts;
+    }
 
     // Notify waiting scripts
     window["SP"] ? window["SP"].SOD.notifyScriptLoadedAndExecuteWaitingJobs("gd-sprest-bs.js") : null;
--- src/rest.ts
+++ src/rest.ts
@@ -2,7 +2,6 @@ import * as jQuery from "jquery";
 import * as Components from "./components";
 import * as WebParts from "./webparts";
 
-export * from "gd-sprest";
 export {
     jQuery,
     Components,
