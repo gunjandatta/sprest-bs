a7e5983311767f93676a972575b4c9ed3a4e40f9
Gunjan Datta <me@dattabase.com>
2018-09-03 01:03:18 -0400

    Fixed the global variable reference to the components.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import { Components, jQuery, WebParts } from "./rest";
+import { WebParts } from "./rest";
 export * from "gd-bs";
 export * from "gd-sprest";
 export * from "./rest";
 //import "./styles.scss";
+declare var GD;
 
 // See if the window exists
 if (window) {
     // Update the $REST global variable
     if (window["$REST"]) {
-        window["$REST"].Components = Components;
-        window["$REST"].jQuery = jQuery;
+        window["$REST"].Components = GD.Components;
+        window["$REST"].jQuery = GD.jQuery;
         window["$REST"].WebParts = WebParts;
     }
 
