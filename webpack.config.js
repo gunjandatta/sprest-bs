0206e23579f39c0d857b483cb027206257282bdf
Gunjan Datta <me@dattabase.com>
2019-01-20 14:30:41 -0500

    Fixed the webpack configuration to output the smallest bundle for dev/prod.

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,13 @@ module.exports = (env, argv) => {
     var isDev = argv.mode === "development";
 
     // Return the configuration
-    return {
-        entry: [
-            "./node_modules/gd-bs/dist/gd-bs" + (isDev ? "" : ".min") + ".js",
-            "./node_modules/gd-sprest/dist/gd-sprest" + (isDev ? "" : ".min") + ".js",
-            "./lib/jquery-ui-1.12.1.custom/jquery-ui" + (isDev ? "" : ".min") + ".js",
-            "./src/index.ts"
-        ],
+    let cfg = {
         output: {
             path: path.resolve(__dirname, "dist"),
             filename: "gd-sprest-bs" + (isDev ? "" : ".min") + ".js"
         },
         resolve: {
             extensions: [".scss", ".css", ".ts", ".js"]
-        },
-        externals: {
-            "gd-bs": "GD",
-            "gd-sprest": "$REST",
-            "$": "GD.jQuery",
-            "jquery": "GD.jQuery"
         },
         module: {
             rules: [
@@ -55,4 +43,34 @@ module.exports = (env, argv) => {
             ]
         }
     };
+
+    // Dev Configuration
+    if (isDev) {
+        cfg.entry = [
+            "./lib/jquery-ui-1.12.1.custom/jquery-ui" + (isDev ? "" : ".min") + ".js",
+            "./src/index.ts"
+        ];
+        cfg.externals = {
+            "$": "GD.jQuery",
+            "jquery": "GD.jQuery"
+        };
+    }
+    // Prod Configuration
+    else {
+        cfg.entry = [
+            "./node_modules/gd-bs/dist/gd-bs" + (isDev ? "" : ".min") + ".js",
+            "./node_modules/gd-sprest/dist/gd-sprest" + (isDev ? "" : ".min") + ".js",
+            "./lib/jquery-ui-1.12.1.custom/jquery-ui" + (isDev ? "" : ".min") + ".js",
+            "./src/index.ts"
+        ];
+        cfg.externals = {
+            "gd-bs": "GD",
+            "gd-sprest": "$REST",
+            "$": "GD.jQuery",
+            "jquery": "GD.jQuery"
+        };
+    }
+
+    // Return the configuration
+    return cfg;
 }
