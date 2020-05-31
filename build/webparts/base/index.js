9bd368d669184f1baa12ea48f3e695ba831f10e2
Gunjan Datta <me@dattabase.com>
2020-05-31 18:32:52 -0400

    Removed the build directory.

--- build/webparts/base/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./helper"));
-__export(require("./wp"));
-__export(require("./wpCfg"));
