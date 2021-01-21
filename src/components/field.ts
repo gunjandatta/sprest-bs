a3389c910de2df9038a19ab2b641b414b531135a
Gunjan Datta <me@dattabase.com>
2021-01-21 16:00:57 -0500

    Bug Fix - The FileLeafRef validation was always set to false.

--- src/components/field.ts
+++ src/components/field.ts
@@ -621,18 +621,19 @@ export const Field = (props: IFieldProps): IField => {
     if (props.field.InternalName == "FileLeafRef") {
         // Set base validation
         baseValidation = (control, result) => {
-            // Ensure the value doesn't contain invalid characters
             let value = result.value;
-            if (value) {
+
+            // Ensure the value exists
+            result.isValid = value ? true : false;
+            if (result.isValid) {
                 // See if it ends w/ a .
                 if (value[value.length - 1] == '.') {
                     // Update the validation
                     result.isValid = false;
                     result.invalidMessage = "The value cannot end with a '.' character.";
                 }
-
-                // See if it contains invalid characters
-                if (/["\#\%\*\:\<\>\?\/\\"]/.test(value) || value.indexOf('\\') >= 0) {
+                // Else, see if it contains invalid characters
+                else if (/["\#\%\*\:\<\>\?\/\\"]/.test(value) || value.indexOf('\\') >= 0) {
                     // Update the validation
                     result.isValid = false;
                     result.invalidMessage = "The value cannot contain the following characters: \" % * : <, > ? / \\ |";
