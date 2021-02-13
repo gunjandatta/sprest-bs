fad89a38401a3548c93fc4adc96c0b2a4aa6a137
Gunjan Datta <me@dattabase.com>
2021-02-13 13:22:46 -0500

    Updated the webpack config to use externals for the core and components library.

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,18 @@ module.exports = (env, argv) => {
 
     // Return the configuration
     var cfg = {
-        entry: "./build/index.js",
+        entry: [
+            "./node_modules/gd-bs/dist/gd-bs-icons" + (isDev ? "" : ".min") + ".js",
+            "./node_modules/gd-sprest/dist/gd-sprest" + (isDev ? "" : ".min") + ".js",
+            "./build/index.js"
+        ],
         output: {
             path: path.resolve(__dirname, "dist"),
             filename: "gd-sprest-bs" + (isDev ? "" : ".min") + ".js"
+        },
+        externals: {
+            "gd-bs": "$REST",
+            "gd-sprest": "$REST"
         },
         module: {
             rules: [
