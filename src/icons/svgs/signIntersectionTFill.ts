89c55ff59a213a56714a12d68832bde54216a4cb
Gunjan Datta <me@dattabase.com>
2024-08-26 10:56:31 -0400

    Updated the components lib.

--- src/icons/svgs/signIntersectionTFill.ts
+++ src/icons/svgs/signIntersectionTFill.ts
@@ -1,4 +1,4 @@
 import { generateIcon } from "../generate";
 export function signIntersectionTFill(height, width, className?) {
-  return generateIcon(`<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-sign-intersection-t-fill' viewBox='0 0 16 16'>   <path d='M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435ZM5 5h6v1.5H8.75V12h-1.5V6.5H5V5Z'/> </svg>`, height, width, className);
+  return generateIcon(`<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-sign-intersection-t-fill' viewBox='0 0 16 16'>   <path d='M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098zM5 5h6v1.5H8.75V12h-1.5V6.5H5z'/> </svg>`, height, width, className);
 }
